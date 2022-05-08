edb9bd187f05554f46fc78568050a27ac4f9847b
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-05-08 07:39:34 -1100

    removed an unused import from _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,6 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <script src="https://smtpjs.com/v3/smtp.js"></script>
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
